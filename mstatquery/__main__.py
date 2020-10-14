3d7f3f08a1057cce2ae60d0587df84476ccf1ff2
Eric Zhao <21zhaoe@protonmail.com>
2020-10-13 22:30:44 -0400

    fix: Move everything in cli.py to __main__

--- mstatquery/__main__.py
+++ mstatquery/__main__.py
@@ -1,8 +1,31 @@
+import argparse
 import sys
 
 from lark import UnexpectedToken
 from mstatquery import lib
-from mstatquery.cli import Parser as CliParser
+
+
+def CliParser(default_encoding: str,
+              default_format_str: str) -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(
+        description='Process a Microsoft Teams meeting attendance tsv.',
+        formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument('file',
+                        help='tsv file to process')
+    parser.add_argument('query', nargs='?',
+                        help='filter query string')
+    parser.add_argument('--encoding', dest='encoding',
+                        default=default_encoding,
+                        help='use an alternate encoding (default %s)'
+                        % (default_encoding))
+    parser.add_argument('--utf16', action='store_true',
+                        help='use utf-16 encoding')
+    parser.add_argument('-f', '--format', default=default_format_str,
+                        help='set format string for each line (default: %s)'
+                        % (default_format_str))
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='enable stack trace')
+    return parser
 
 
 def main():
